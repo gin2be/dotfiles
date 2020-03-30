857eb74e0d8482ef05e137b26c3416136b22b146
gin2be <ag.you2be@gmail.com>
2020-03-30 18:59:45 +0900

    wip

--- install.sh
+++ install.sh
@@ -2,59 +2,80 @@
 
 set -e
 
-DOTFILES_REPO="https://github.com/gin2be/dotfiles"
-
-DOTFILES=${DOTFILES:="$HOME/.dotfiles"}
-
-function is_exists() {
-  type "$1" 1>/dev/null 2>&1
+function log_failure() {
+  echo -e "\033[31m✗\033[00m \033[01m$@\033[00m"
 }
 
-#function sudo() {
-#  if [ "${ROOT_PASSWORD}" = "" ]; then
-#    echo
-#    read -sp "Please input your password: " ROOT_PASSWORD
-#    echo
-#  fi
-#  echo ${ROOT_PASSWORD} | $(which sudo) -S $@
-#}
+function log_success() {
+  echo -e "\033[32m✔\033[00m \033[01m$@\033[00m"
+}
 
-function download_dotfiles() {
-  if [ ! -e $(dirname ${DOTFILES}) ]; then
-    mkdir -p $(dirname ${DOTFILES})
-  fi
-  if is_exists git; then
-    if [ -e ${DOTFILES} ]; then
-      echo " ${DOTFILES} already exists. Updating."
-      cd ${DOTFILES}; git pull --prune
-    else
-      git clone ${DOTFILES_REPO} ${DOTFILES}
-    fi
-  elif is_exists curl || is_exists wget; then
-    if [ -e ${DOTFILES} ]; then
-      echo " ${DOTFILES} already exists. Updating."
-      rm -rf ${DOTFILES}
-    fi
-    archive=${DOTFILES_REPO}/archive/master.tar.gz
-    if is_exists curl; then
-      curl -L ${archive} | tar zxv
-    elif is_exists wget; then
-      wget -O- ${archive} | tar zxv
-    fi
-    mv dotfiles-master ${DOTFILES}
+function spinner() {
+  local pid=$1; shift
+  local message=$@
+  local chars="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
+  local delay=0.2
+
+  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+    for (( i=0; i<${#chars}; i++ )); do
+      echo -en "${chars:$i:1} " "$message" "\r"
+      sleep $delay
+    done
+  done
+  if wait $pid; then
+    log_success "$message"
   else
-    echo -e "\\ngit, curl or wget required."
-    exit 1
+    log_failure "$message"
   fi
 }
 
-echo "Installing dotfiles..."
-echo "=============================="
+function longprocess() {
+  sleep 1
+}
 
-echo -e "\\nDownloading dotfiles repository..."
-download_dotfiles
+(longprocess 1>/dev/null 2>&1) &
+spinner $! wait 2 seconds
 
-source ${DOTFILES}/scripts/link.sh
-source ${DOTFILES}/scripts/middlewares.sh
+#DOTFILES_REPO="https://github.com/gin2be/dotfiles"
+#
+#DOTFILES=${DOTFILES:="$HOME/.dotfiles"}
 
-echo
+#function download_dotfiles() {
+#  if [ ! -e $(dirname ${DOTFILES}) ]; then
+#    mkdir -p $(dirname ${DOTFILES})
+#  fi
+#  if is_exists git; then
+#    if [ -e ${DOTFILES} ]; then
+#      echo " ${DOTFILES} already exists. Updating."
+#      cd ${DOTFILES}; git pull --prune
+#    else
+#      git clone ${DOTFILES_REPO} ${DOTFILES}
+#    fi
+#  elif is_exists curl || is_exists wget; then
+#    if [ -e ${DOTFILES} ]; then
+#      echo " ${DOTFILES} already exists. Updating."
+#      rm -rf ${DOTFILES}
+#    fi
+#    archive=${DOTFILES_REPO}/archive/master.tar.gz
+#    if is_exists curl; then
+#      curl -L ${archive} | tar zxv
+#    elif is_exists wget; then
+#      wget -O- ${archive} | tar zxv
+#    fi
+#    mv dotfiles-master ${DOTFILES}
+#  else
+#    echo -e "\\ngit, curl or wget required."
+#    exit 1
+#  fi
+#}
+#
+#echo "Installing dotfiles..."
+#echo "=============================="
+#
+#echo -e "\\nDownloading dotfiles repository..."
+#download_dotfiles
+#
+#source ${DOTFILES}/scripts/link.sh
+#source ${DOTFILES}/scripts/middlewares.sh
+#
+#echo
