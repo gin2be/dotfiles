e56cf9b571cc7c8820f0c5a84157aeb09db961b8
gin2be <ag.you2be@gmail.com>
2020-04-11 00:20:18 +0900

    Add docker-compose

--- scripts/middlewares/tools.sh
+++ scripts/middlewares/tools.sh
@@ -4,3 +4,7 @@ echo " installing tools"
 
 echo "  installing heroku"
 curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+
+echo "  installing docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
